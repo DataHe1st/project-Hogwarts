1dbe4fae8eae36c3366e5522e0933a3974e094f7
Srishty Mangutte <srishtymangutte@gmail.com>
2023-08-20 08:51:16 +0530

    feat: added sponsors and button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-
+import React from 'react';
 import './App.css';
 import { UnderC } from './components/UnderC';
 
+
 function App() {
+  React.useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+}, []);
+
   return (
     <UnderC/>
   );
---
+++ src/components/Sponsors.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/sponsors.styles.css';
+import devf from '../images/devf.png';
+import filec from '../images/filecon.jpg';
+import polyg from '../images/polyg.png';
+import replit from '../images/replit.jpg';
+import solana from '../images/solana.png';
+
+export const Sponsors = () => {
+  return (
+    <div className='sponsor-cont'>
+        <div className='spon'><img alt='sponsor' src={devf} /></div>
+        <div className='spon'><img alt='sponsor' src={filec} /></div>
+        <div className='spon'><img alt='sponsor' src={polyg} /></div>
+        <div className='spon'><img alt='sponsor' src={replit} /></div>
+        <div className='spon'><img alt='sponsor' src={solana} /></div>
+    </div>
+  )
+}
